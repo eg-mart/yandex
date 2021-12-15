f901d2ab2f982d1aced9d7d2990c7a6b684b1679
eg-mart <marteg71@gmail.com>
2021-12-15 10:37:26 +0400

    hhh

--- 2.py
+++ 2.py
@@ -1,55 +1,96 @@
-import csv
-import sys
-
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
-from PyQt5.QtWidgets import QHeaderView
-from PyQt5.QtCore import Qt
+import pygame
+from copy import deepcopy
 
 
-class Main(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('table.ui', self)
-        self.load_table()
-        self.update1()
-        self.tableWidget.itemChanged.connect(self.update1)
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        self.left = 10
+        self.top = 10
+        self.cell_size = 20
+        self.color = 'green'
 
-    def load_table(self):
-        with open('price.csv', encoding="utf8") as csvfile:
-            reader = csv.reader(csvfile,
-                                delimiter=';', quotechar='"')
-            title = next(reader)
-            self.tableWidget.setColumnCount(len(title) + 1)
-            self.tableWidget.setHorizontalHeaderLabels(title + ['Количество'])
-            self.tableWidget.setRowCount(0)
-            for i, row in enumerate(reader):
-                self.tableWidget.setRowCount(
-                    self.tableWidget.rowCount() + 1)
-                self.tableWidget.setItem(i, 2, QTableWidgetItem('0'))
-                for j, elem in enumerate(row):
-                    item = QTableWidgetItem(elem)
-                    item.setFlags(Qt.ItemIsEnabled)
-                    self.tableWidget.setItem(i, j, item)
-        self.tableWidget.resizeColumnsToContents()
-        header = self.tableWidget.horizontalHeader()
-        header.setSectionResizeMode(0, QHeaderView.Stretch)
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
 
-    def update1(self):
-        try:
-            res = 0
-            for i in range(self.tableWidget.rowCount()):
-                cost = int(self.tableWidget.item(i, 1).text())
-                amount = int(self.tableWidget.item(i, 2).text())
-                res += cost * amount
-            self.allCost.setText(str(res))
-        except ValueError:
-            self.allCost.setText('Неверный формат данных')
+    def render(self, surface):
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.board[y][x]:
+                    pygame.draw.rect(surface, self.color,
+                                     (self.left + x * self.cell_size, self.top + y * self.cell_size,
+                                      self.cell_size, self.cell_size))
+                pygame.draw.rect(surface, 'white',
+                                 (self.left + x * self.cell_size, self.top + y * self.cell_size,
+                                  self.cell_size, self.cell_size), 1)
+
+    def get_cell(self, mouse_pos):
+        pos = [mouse_pos[0] - self.left, mouse_pos[1] - self.top]
+        if 0 <= pos[0] <= self.cell_size * self.width and \
+                0 <= pos[1] <= self.cell_size * self.height:
+            pos[0] = pos[0] // self.cell_size
+            pos[1] = pos[1] // self.cell_size
+            return pos[0], pos[1]
+        return None
+
+    def on_click(self, cell_coords):
+        pass
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        self.on_click(cell)
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    main = Main()
-    main.show()
-    sys.exit(app.exec())
+class Life(Board):
+    def on_click(self, cell_coords):
+        new_value = abs(self.board[cell_coords[1]][cell_coords[0]] - 1)
+        self.board[cell_coords[1]][cell_coords[0]] = new_value
+
+    def next_move(self):
+        prev_state = deepcopy(self.board)
+        for y in range(self.height):
+            for x in range(self.width):
+                neighbours = 0
+                for i in range(-1, 2):
+                    for j in range(-1, 2):
+                        if i != 0 or j != 0:
+                            neighbours += prev_state[(y + i) % self.height][(x + j) % self.width]
+                if neighbours == 3:
+                    self.board[y][x] = 1
+                elif neighbours != 2:
+                    self.board[y][x] = 0
+
+
+pygame.init()
+screen = pygame.display.set_mode((630, 430))
+board = Life(30, 20)
+running = True
+edit_mode = True
+clock = pygame.time.Clock()
+fps = 60
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == pygame.BUTTON_LEFT and edit_mode:
+                board.get_click(event.pos)
+            elif event.button == pygame.BUTTON_RIGHT:
+                edit_mode = not edit_mode
+            elif event.button == pygame.BUTTON_WHEELDOWN:
+                fps = max(fps - 5, 1)
+            elif event.button == pygame.BUTTON_WHEELUP:
+                fps += 5
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                edit_mode = not edit_mode
+    if not edit_mode:
+        board.next_move()
+    screen.fill((0, 0, 0))
+    board.render(screen)
+    pygame.display.flip()
+    clock.tick(fps)
--- nm.py
+++ nm.py
@@ -1,50 +1,30 @@
-class AirCastle:
-    def __init__(self, height, blocks, color):
-        self.height = height
-        self.blocks = blocks
-        self.color = color
+import sys
 
-    def change_height(self, val):
-        self.height = max(self.height + val, 0)
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sqlite3
 
-    def __iadd__(self, other):
-        self.blocks += other
-        self.height += other // 5
-        return self
 
-    def __call__(self, *args, **kwargs):
-        return self.height // args[0] * self.blocks
+class Main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('films.ui', self)
+        self.btn_search.clicked.connect(self.search)
+        self.load_labels()
 
-    def __str__(self):
-        return f'The AirCastle at an altitude of ' \
-            f'{self.height} meters is {self.color} with {self.blocks} clouds.'
+    def load_labels(self):
+        con = sqlite3.connect('films_db.sqlite')
+        cur = con.cursor()
+        genres = cur.execute('SELECT title FROM genres').fetchall()
+        for genre in genres:
+            self.comboBox.addItem(genre[0])
 
-    def __eq__(self, other):
-        if self.blocks == other.blocks and self.height == other.height and self.color == other.color:
-            return True
-        return False
+    def search(self):
+        pass
 
-    def __ne__(self, other):
-        return not self == other
 
-    def __lt__(self, other):
-        if self.blocks > other.blocks:
-            return False
-        elif self.blocks == other.blocks:
-            if self.height > other.height:
-                return False
-            elif self.height == other.height:
-                if self.color > other.color:
-                    return False
-                elif self.color == other.color:
-                    return False
-        return True
-
-    def __gt__(self, other):
-        return not self < other and not self == other
-
-    def __le__(self, other):
-        return self < other or self == other
-
-    def __ge__(self, other):
-        return self > other or self == other
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = Main()
+    main.show()
+    sys.exit(app.exec())

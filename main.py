73240760ff422eb4d63a492019c3163694e080eb
eg-mart <marteg71@gmail.com>
2022-04-13 11:42:18 +0400

    Finished first step

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from forms import LoginForm
 from flask import Flask, url_for, render_template
+from Jobs import Jobs
+from User import User
+from db_session import create_session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -9,6 +12,20 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def login():
     form = LoginForm()
     return render_template('login.html', form=form)
+
+
+@app.route('/jobs-table')
+def job_monitor():
+    session = create_session()
+    with session.begin():
+        jobs = session.query(Jobs).all()
+        job = session.query(Jobs).first()
+        user = User()
+        user.name = 'Me'
+        user.surname = 'You'
+        session.add(user)
+        job.team_leader = user
+    return render_template('jobs_monitor.html', jobs=jobs)
 
 
 if __name__ == '__main__':
--- tester.py
+++ tester.py
@@ -1,3 +1,8 @@
 from db_session import create_session
+from Jobs import Jobs
+from User import User
 
 s = create_session()
+usr = User()
+job = Jobs()
+job.team_leader = usr
